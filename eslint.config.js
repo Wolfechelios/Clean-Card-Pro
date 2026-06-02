0e30e0dda984f47abcef72cfb2909d162d71f024
WolfeChelios <132318325+Wolfechelios@users.noreply.github.com>
2026-06-01 18:04:00 -0700

    Ignore generated and platform build folders in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,18 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  {
+    ignores: [
+      "dist",
+      "dist-ssr",
+      "node_modules",
+      ".next",
+      ".vercel",
+      "coverage",
+      "android",
+      "ios",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
